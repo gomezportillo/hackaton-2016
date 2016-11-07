39f6a27bb530f2dfd2b163a54fc05a5054d99a4b
gomezportillo <pedromanuel.gomezportillo@alu.uclmes>
2016-11-07 19:18:32 +0100

    no entiendo que he hecho pero va

--- main.py
+++ main.py
@@ -1,18 +1,49 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
 
+def check_sheldonness(number):
+    binary = "{0:b}".format(number)
+
+    M = 0
+    N = 0
+
+    M = len(filter(None, str(binary).split('0'))[0])
+
+    try:
+        N = len(filter(None, str(binary).split('1'))[0])
+    except IndexError:
+        N = 0
+
+    lM = filter(None, str(binary).split('0'))
+    for e in lM:
+        if len(e) != M:
+            return False
+
+    try:
+        lN = filter(None, str(binary).split('1'))
+    except IndexError:
+        return False
+
+    for e in lN:
+        if len(e) != N:
+            return False
+
+    return True
+
+    print "{} {} {}".format(binary, M, N)
+
 def main():
-    empresas = []
-    alumnos = []
-
-    with open('04.in') as f:
+    lines = None
+    with open('11.in') as f:
         lines = f.readlines()
-        empresas = lines[0].split(',')
-        alumnos = lines[1].split(',')
+        lines = lines[0].split()
 
-    empresas[4] = empresas[4].replace('\n', '')
-    alumnos[3] = alumnos[3].replace('\n', '')
+    sheldon_numbers = []
+    for number in range(int(lines[0]), int(lines[1])+1):
+            if check_sheldonness(number):
+                sheldon_numbers.append(number)
 
-
+    with open('11.out', 'w') as f:
+        f.write(str(sheldon_numbers))
 
 main()
