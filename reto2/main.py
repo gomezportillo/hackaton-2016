6cf083e0f109cf95ec1dab72da6e723ad5c7a020
jcepedavillamayor <jcepedavillamayor@gmail.com>
2016-11-07 11:38:58 +0100

    Completed

---
+++ reto2/main.py
@@ -0,0 +1,57 @@
+from math import sqrt
+
+def split_text(text):
+    text_without_spaces = text.replace(" ", "")
+    # print(text_without_spaces)
+    return text_without_spaces
+
+def set_text(text):
+    new_text = set(text)
+    return new_text
+
+def count_occurences_and_obtain_ascii(text, setted_text):
+    occurences = {}
+    for word in setted_text:
+        occurences[word] = (text.count(word), ord(word))
+    return occurences
+
+def obtain_prime_chars(occurences):
+    primes = {}
+    for key, value in occurences.items():
+        n = obtain_prime(value[0])
+        if 0 <= n <= 1:
+            primes[key] = value
+    return primes
+
+def obtain_prime(number):
+    divisors = 0
+    if number % 2 == 0:
+        return 900
+    for num in range(1, int(sqrt(number))):
+        divisors += 1 if number % num == 0 else 0
+    return divisors
+
+def calc_prices(primes):
+    prices = {}
+    for key, value in primes.items():
+        prices[key] = (value[0], obtain_price(value[1], value[0]))
+    return prices
+
+def obtain_price(code, occurence):
+    return round(code/1000 * float(occurence), 3)
+
+def print_results(prices):
+    total = 0.0
+    for key, value in prices.items():
+        total += value[1]
+        print("{} {} {}€".format(key, value[0], value[1]))
+    print("Coste total: {}€".format(total))
+
+
+file = open("02.in", 'r')
+text = split_text(file.read())
+setted_text = set_text(text)
+occurences = count_occurences_and_obtain_ascii(text, setted_text)
+primes = obtain_prime_chars(occurences)
+prices = calc_prices(primes)
+print_results(prices)
