87bf5acb271056c009bc5bb856eb55c7a7386a7d
jcepedavillamayor <jcepedavillamayor@gmail.com>
2016-11-07 18:52:44 +0100

    Done

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
+import itertools
 
 def main():
     empresas = []
@@ -12,7 +13,18 @@ def main():
 
     empresas[4] = empresas[4].replace('\n', '')
     alumnos[3] = alumnos[3].replace('\n', '')
+    alumnos.append('Empresa vacia')
 
+    combinations = itertools.permutations(alumnos, len(alumnos))
+    for i in combinations:
+        if check_validity(i):
+            print(i)
 
+def check_validity(alumnos):
+    p = alumnos.index("Alumno P")
+    q = alumnos.index(" Alumno Q")
+    r = alumnos.index(" Alumno R")
+    t = alumnos.index(" Alumno T")
+    return abs(p - q) > 1 and abs(r - t) > 2
 
 main()
