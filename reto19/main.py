98ac46a6936508f2058e2f2ae3e496cafabf2e8b
jcepedavillamayor <jcepedavillamayor@gmail.com>
2016-11-07 13:41:11 +0100

    hecho otra vez

--- reto19/main.py
+++ reto19/main.py
@@ -1,9 +1,9 @@
 import json
+from collections import OrderedDict
 
 file = open("19.in",'r')
 
 text = file.read()
 
-json_loaded = json.loads(text)
-
+json_loaded = json.loads(text, object_pairs_hook=OrderedDict)
 print(json.dumps(json_loaded, indent=2))
